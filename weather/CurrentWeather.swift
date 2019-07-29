a572a9607d2362ecfdae7756703dd7813c60404e
Sanchikuuus <Sanchikuuus@ukr.net>
2019-07-29 22:13:50 +0300

    added CurrentWeather structure added updateUI func 'n test data

---
+++ weather/CurrentWeather.swift
@@ -0,0 +1,84 @@
+//
+//  CurrentWeather.swift
+//  weather
+//
+//  Created by Sashko Shel on 7/29/19.
+//  Copyright © 2019 Sashko Shel. All rights reserved.
+//
+
+import UIKit
+
+struct CurrentWeather {
+    let icon: UIImage
+    let presure: Double
+    let humidity: Double
+    let temperature: Double
+    let appearentTemperature: Double
+}
+
+enum WeatherIconManager: String {
+    
+    case ClearDay = "clear-day"
+    case ClearNight = "clear-night"
+    case Rain = "rain"
+    case Snow = "snow"
+    case Sleet = "sleet"
+    case Wind = "wind"
+    case Fog = "fog"
+    case Cloudy = "cloudy"
+    case PartlyCloudyDay = "partly-cloudy-day"
+    case PartlyCloudyNight = "partly-cloudy-night"
+    case UnpredictedIcon = "unpredicted-icon"
+    
+    init(rawValue: String) {
+        switch rawValue {
+        case "clear-day":
+            self = .ClearDay
+        case "clear-night":
+            self = .ClearNight
+        case "rain":
+            self = .Rain
+        case "snow":
+            self = .Snow
+        case "sleet":
+            self = .Sleet
+        case "wind":
+            self = .Wind
+        case "fog":
+            self = .Fog
+        case "cloudy":
+            self = .Cloudy
+        case "partly-cloudy-day":
+            self = .PartlyCloudyDay
+        case "partly-cloudy-night":
+            self = .PartlyCloudyNight
+        default:
+            self = .UnpredictedIcon
+            
+        }
+    }
+}
+
+extension WeatherIconManager {
+    var image: UIImage {
+        return UIImage(named: self.rawValue)!
+    }
+}
+
+extension CurrentWeather {
+    var presureString: String {
+        return "\(Int(presure)) mm"
+    }
+    
+    var humidityString: String {
+        return "\(Int(humidity)) %"
+    }
+    
+    var temperatureString: String {
+        return "\(temperature)°C"
+    }
+    
+    var appearentTemperatureString: String {
+        return "Feels like \(appearentTemperature)°C"
+    }
+}
--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var presureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    private func updateUIWith(_ currentWeather: CurrentWeather) {
+        imageView.image = currentWeather.icon
+        presureLabel.text = currentWeather.presureString
+        temperatureLabel.text = currentWeather.temperatureString
+        appearentTemperatureLabel.text = currentWeather.appearentTemperatureString
+        humidityLabel.text =  currentWeather.humidityString
     }
 
-
+    @IBAction func refreshButtonTapped(_ sender: Any) {
+        let icon = WeatherIconManager.Rain.image
+        let currentWeather = CurrentWeather(icon: icon, presure: 750, humidity: 60, temperature: 10.0, appearentTemperature: 8)
+        updateUIWith(currentWeather)
+    }
+    
 }
 
