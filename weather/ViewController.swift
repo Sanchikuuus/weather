272b5ddce3bdc4fa1b4d85e9131b74d56b859e1a
Sanchikuuus <Sanchikuuus@ukr.net>
2019-07-31 21:01:45 +0300

    start from beginning

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -15,38 +15,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var presureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var apparentTemperatureLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
+    
+//    lazy var weatherManager = APIWeatherManager(apiKey: "8ceb75b92983c3e34fc681d0539183ab")
+//    let coordinates = Coordinates(latitude: 50.501885, longitude: 30.606308)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-//        let baseURL = URL(string: "https://api.darksky.net/forecast/8ceb75b92983c3e34fc681d0539183ab/") //base URL for getting data
-//        let fullURL = URL(string: "37.8267,-122.4233", relativeTo: baseURL) // full URL, adding location to base URL
-//        
-//        let sessionConfiguration = URLSessionConfiguration.default
-//        let session = URLSession(configuration: sessionConfiguration)
-//
-//        let request = URLRequest(url: fullURL!)
-//        let dataTask = session.dataTask(with: fullURL!) { (data, response, error) in
-//
-//        } // get data
-//        dataTask.resume() //
+
+    
     }
     
     private func updateUIWith(_ currentWeather: CurrentWeather) {
         imageView.image = currentWeather.icon
         presureLabel.text = currentWeather.presureString
         temperatureLabel.text = currentWeather.temperatureString
-        appearentTemperatureLabel.text = currentWeather.appearentTemperatureString
+        apparentTemperatureLabel.text = currentWeather.apparentTemperatureString
         humidityLabel.text =  currentWeather.humidityString
     }
 
     @IBAction func refreshButtonTapped(_ sender: Any) {
-        let icon = WeatherIconManager.Rain.image
-        let currentWeather = CurrentWeather(icon: icon, presure: 750, humidity: 60, temperature: 10.0, appearentTemperature: 8)
-        updateUIWith(currentWeather)
+       
+       
     }
     
 }
